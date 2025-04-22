2e56f90733e521641e6bb795a923b31008006c1e
matthew <coltoiu.matei@gmail.com>
2025-04-23 00:58:42 +0300

    Fix startup crash by executing notebook before Voilà

--- start.py
+++ start.py
@@ -1,3 +1,9 @@
 import os
+import time
+
+print("✅ Execut notebookul în fundal...")
+os.system("jupyter nbconvert --execute --inplace Sudoku_3D_Voila_Final.ipynb")
+
+print("🟢 Lansăm Voilà...")
 os.system("voila Sudoku_3D_Voila_Final.ipynb --port=$PORT --no-browser --theme=dark --strip_sources=True")
 
